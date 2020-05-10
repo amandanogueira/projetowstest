603b6028d1d44cab94f30b458db2918a05d354e6
unknown <amanda.n.93@hotmail.com>
2020-05-10 17:26:27 -0300

    primeiro commit

--- src/main/java/br/rs/ClienteRest.java
+++ src/main/java/br/rs/ClienteRest.java
@@ -25,7 +25,7 @@ public class ClienteRest {
 
     private WebTarget webTarget;
     private Client client;
-    private static final String BASE_URI = "http://localhost:8080/projetowstest/resources";
+    private static final String BASE_URI = "http://localhost/projetowstest/resources";
 
     public ClienteRest() {
         client = javax.ws.rs.client.ClientBuilder.newClient();
